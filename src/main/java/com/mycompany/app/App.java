be1cc0c66fecc7b42469a59b218087cef2080ebb
bikal77 <93905679+bikal77@users.noreply.github.com>
2024-11-24 23:29:16 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,9 @@
  package com.mycompany.app;
 //* Hello world!!
- * *
- * Author: Bikal KC
- * Student ID: C0929675
- * Date of Submission: Nov 27th, 2024
+// * *
+// * Author: Bikal KC
+ //* Student ID: C0929675
+ //* Date of Submission: Nov 27th, 2024
  
 public class App
 {
